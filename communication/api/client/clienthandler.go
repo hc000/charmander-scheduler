73e38b953dbc95b7b4ced381977b91383998b9da
hc000 <hc000@users.noreply.github.com>
2015-03-10 08:25:28 -0700

    Update clienthandler.go
    enabled CORS

--- communication/api/client/clienthandler.go
+++ communication/api/client/clienthandler.go
@@ -38,7 +38,10 @@ type ClientHandler struct {
 
 func (self *ClientHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 	path := html.EscapeString(request.URL.Path)
-	if strings.HasSuffix(path, "/task") && request.Method == "POST" {
+	responseWriter.Header().Set("Access-Control-Allow-Origin", origin)
+        responseWriter.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        responseWriter.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")	
+        if strings.HasSuffix(path, "/task") && request.Method == "POST" {
 		defer request.Body.Close()
 
 		task := &manager.Task{}
